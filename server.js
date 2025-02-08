fa264f468aea8adf225fe160ce932aae6503125f
aman dubey <dubeyaman1209@gmail.com>
2025-02-08 13:05:36 +0530

    mark update

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(express.json());
-app.use(cors());
+app.use(express.json({limit:"100mb"}));
+app.use(express.urlencoded({limit:"10mb",extended:true}));
+app.use(cors({origin:"*", credentials: true}));
+
 app.use(morgan("dev"));
 
 // Routes
